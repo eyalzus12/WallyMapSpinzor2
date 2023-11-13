27b2e5aa6ba311870f44eaff3b22f8c9de5db8b5
Eyal Zusiman <eyal.zusiman@gmail.com>
2023-11-13 13:58:25 +0200

    missed a thing

--- src/Maps/Bounds/SpawnBotBounds.cs
+++ src/Maps/Bounds/SpawnBotBounds.cs
@@ -4,26 +4,26 @@ namespace WallyMapSpinzor2;
 
 public class SpawnBotBounds : IDeserializable, ISerializable, IDrawable
 {
+    public double H{get; set;}
+    public double W{get; set;}
     public double X{get; set;}
     public double Y{get; set;}
-    public double W{get; set;}
-    public double H{get; set;}
     public void Deserialize(XElement element)
     {
+        H = element.GetFloatAttribute("H");
+        W = element.GetFloatAttribute("W");
         X = element.GetFloatAttribute("X");
         Y = element.GetFloatAttribute("Y");
-        W = element.GetFloatAttribute("W");
-        H = element.GetFloatAttribute("H");
     }
 
     public XElement Serialize()
     {
         XElement e = new("SpawnBotBounds");
 
+        e.SetAttributeValue("H", H.ToString());
+        e.SetAttributeValue("W", W.ToString());
         e.SetAttributeValue("X", X.ToString());
         e.SetAttributeValue("Y", Y.ToString());
-        e.SetAttributeValue("W", W.ToString());
-        e.SetAttributeValue("H", H.ToString());
 
         return e;
     }
