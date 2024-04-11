0187be38221cad219a744578b66bc8dd72c2aa76
Eyal Zusiman <eyal.zusiman@gmail.com>
2024-04-11 11:27:09 +0300

    update to patch 8.07

--- src/Maps/LevelDesc/Collision/LavaCollision.cs
+++ src/Maps/LevelDesc/Collision/LavaCollision.cs
@@ -1,7 +1,23 @@
+using System.Xml.Linq;
+
 namespace WallyMapSpinzor2;
 
 public class LavaCollision : AbstractCollision
 {
+    public string LavaPower { get; set; } = null!;
+
+    public override void Deserialize(XElement e)
+    {
+        base.Deserialize(e);
+        LavaPower = e.GetAttribute("LavaPower");
+    }
+
+    public override void Serialize(XElement e)
+    {
+        e.SetAttributeValue("LavaPower", LavaPower);
+        base.Serialize(e);
+    }
+
     public override Color GetColor(RenderConfig config) => config.ColorLavaCollision;
     public override CollisionTypeEnum CollisionType => CollisionTypeEnum.HARD | CollisionTypeEnum.GAMEMODE | CollisionTypeEnum.LAVA;
 }
